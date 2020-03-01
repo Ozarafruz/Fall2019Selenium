1cfa138027720b70723f4d1876212db20f4ce7bb
ozarafruz <ozarafruz@gmail.com>
2020-03-01 13:57:07 -0500

    Basic open and close WebDriver

--- src/test/java/com/automation/test/day2/BasicNavigation.java
+++ src/test/java/com/automation/test/day2/BasicNavigation.java
@@ -19,8 +19,24 @@ public class BasicNavigation {
         driver.get("http://google.com");//to open a website
 
         Thread.sleep(3000); //for demo, ait 3 seconds
+        //method that return page title
+        //you can also see it as tab name, in the browser
 
+        String title = driver.getTitle();// returns <title>Some titile</title> text
+
+        String expectedTitle = "Google";
+
+        System.out.println("title = " + title);
+
+        if (expectedTitle.equals(title)){
+            System.out.println("TEST PASSED");
+        }else {
+            System.out.println("TEST FAILED");
+        }
+
+        //MUST BE AT THE END
         driver.close();//to close browser
+        //browser cannot cole itself
 
 
 
