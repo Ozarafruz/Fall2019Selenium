1493387a1424ab5c3218d07aca852d0333708d9e
ozarafruz <ozarafruz@gmail.com>
2020-03-07 17:53:50 -0500

    Registration Form practice

--- src/test/java/com/automation/test/day5_Radio_Buttons/RegistrationForm.java
+++ src/test/java/com/automation/test/day5_Radio_Buttons/RegistrationForm.java
@@ -24,14 +24,23 @@ public class RegistrationForm {
         driver.findElement(By.name("email")).sendKeys("jsmith@email.com");
         driver.findElement(By.name("password")).sendKeys("supersecretpassword2020");
         driver.findElement(By.name("phone")).sendKeys("571-343-2342");
+
         List<WebElement> genders = driver.findElements(By.name("gender"));
         //select gender
-        genders.get(1).click();//select male, for example
+        genders.get(0).click();//select male, for example
+
         driver.findElement(By.name("birthday")).sendKeys("01/01/2007");
-        driver.findElement(By.id("inlineCheckbox2")).click();
+
+        driver.findElement(By.id("inlineCheckbox2")).click();//select java
+
         BrowserUtils.wait(2);
-        driver.findElement(By.id("wooden_spoon")).click();
+        driver.findElement(By.id("wooden_spoon")).click();//click on submit button
         BrowserUtils.wait(2);
+
+
+        //ADD VALIDATION PART
+
+
         driver.quit();
 
 
