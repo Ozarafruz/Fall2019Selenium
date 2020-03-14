070be3c388cb015818b52314ad915a32433b5d29
ozarafruz <ozarafruz@gmail.com>
2020-03-14 18:02:49 -0400

    TestNG, Assertion, (Unit Testing) Practice.com

--- src/test/java/com/automation/test/day8_TestNG/PracticeTests.java
+++ src/test/java/com/automation/test/day8_TestNG/PracticeTests.java
@@ -5,6 +5,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -12,6 +13,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 /**
  * Create a class called PracticeTests
@@ -50,6 +53,36 @@ public class PracticeTests {
         //if assertion fails - it will throw exception and message will be printed
         //3 parameters: (expected, actual, "message in case of error")
         Assert.assertEquals(actual, expected, "Sub-header message is not matching!");
+    }
+    @Test
+    public void forgotPassword(){
+        driver.findElement(By.linkText("Forgot Password")).click();
+        BrowserUtils.wait(3);
+        driver.findElement(By.name("email")).sendKeys("john@gmail.com",Keys.ENTER);
+        BrowserUtils.wait(3);
+        String expected = "Your e-mail's been sent!";
+        String actual = driver.findElement(By.name("confirmation_message")).getText();
+
+        // String actual = driver.findElement(By.tagName("h4")).getText();
+        Assert.assertEquals(actual,expected,"Conformation message is not valid");
+
+    }
+    @Test
+    public void checkBoxTest1(){
+        driver.findElement(By.linkText("Checkboxes")).click();
+
+        //locator for specific checkbox, xpath: //input[1], cssSelector: input:nth-of-type(1)
+        //  //input[@type="checkbox"][1]
+        //collect all checkboxes
+        List<WebElement> checkboxes = driver.findElements(By.tagName("input"));
+        BrowserUtils.wait(4);
+
+        checkboxes.get(0).click();//to click on 1st checkbox
+
+        Assert.assertTrue(checkboxes.get(0).isSelected(), "Checkbox #1 is not selected!");
+
+        BrowserUtils.wait(4);
+
     }
 
 
