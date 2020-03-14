b34646c3c8cb76dec5da7bd927f88ab2a5247f04
ozarafruz <ozarafruz@gmail.com>
2020-03-14 12:53:06 -0400

    @Test ( Assert Practice )

--- src/test/java/com/automation/test/day8_TestNG/BasicTestNGTests.java
+++ src/test/java/com/automation/test/day8_TestNG/BasicTestNGTests.java
@@ -1,10 +1,26 @@
 package com.automation.test.day8_TestNG;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class BasicTestNGTests {
 
+@Test
+public void test1(){
 
 
+    String expected = "apple";
+    String actual = "apple";
+    Assert.assertEquals(actual,expected);
+}
+@Test
+public void test2(){
 
-
+    int num1 = 5 ;
+    int num2 =10 ;
+     //it calls hard assertion
+    //if assertion fails - it stop the execution (due to exception)
+    Assert.assertTrue(num1<num2);
+}
 
 }
