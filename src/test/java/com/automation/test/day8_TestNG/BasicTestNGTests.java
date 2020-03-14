e807588865fb8eb6a2fd39cb0de93f03694724c9
ozarafruz <ozarafruz@gmail.com>
2020-03-14 13:21:10 -0400

    ( Assert Practice ) @Test,@BeforeMethod, @AfterMethod

--- src/test/java/com/automation/test/day8_TestNG/BasicTestNGTests.java
+++ src/test/java/com/automation/test/day8_TestNG/BasicTestNGTests.java
@@ -1,14 +1,29 @@
 package com.automation.test.day8_TestNG;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class BasicTestNGTests {
 
+    //RUNS BEFORE EVERY TEST AUTOMATICALLY
+    //WORKS AS A pre-condition or setup
+    @BeforeMethod
+    public void setup(){
+        System.out.println("BEFORE METHOD");
+    }
+    //Runs automatically after every test
+    @AfterMethod
+    public void teardown(){
+        System.out.println("AFTER METHOD");
+    }
+
+
 @Test
 public void test1(){
 
-
+    System.out.println("TEST1");
     String expected = "apple";
     String actual = "apple";
     Assert.assertEquals(actual,expected);
@@ -16,6 +31,7 @@ public void test1(){
 @Test
 public void test2(){
 
+    System.out.println("TEST2");
     int num1 = 5 ;
     int num2 =10 ;
      //it calls hard assertion
