09c221cc5ef8a06044dcaf9201b592a3daaa565f
ozarafruz <ozarafruz@gmail.com>
2020-03-15 13:29:02 -0400

    TestNG Vytrack Login

--- src/test/java/com/automation/test/vytrack/login/LoginPageTests.java
+++ src/test/java/com/automation/test/vytrack/login/LoginPageTests.java
@@ -22,63 +22,66 @@ public class LoginPageTests {
     //https - data encrypted, no chance for hackers to retrieve info
     //http - data as a plain text, very easy to hack it
     private String URL = "https://qa2.vytrack.com/user/login";
-    //CREDENTIALS FOR store manager
-    private String username= "storemanager85";
+    //    CREDENTIALS FOR store manager
+    private String username = "storemanager85";
     private String password = "UserUser123";
 
     private By usernameBy = By.id("prependedInput");
-    private By passwordBy = By.id("prependedInput");
+    private By passwordBy = By.id("prependedInput2");
     // > in css selector means same thing as / in xpath - direct child
-    private By warningMessageBy = By.className("[class='alert alert-error'] > div");
+    private By warningMessageBy = By.cssSelector("[class='alert alert-error'] > div");
 
     @Test(description = "Verify that warning message displays when user enters invalid username")
     public void invalidUsername(){
         driver.findElement(usernameBy).sendKeys("invalidusername");
         driver.findElement(passwordBy).sendKeys("UserUser123", Keys.ENTER);
-        BrowserUtils.wait(3);
+        BrowserUtils.wait(5);
+
         WebElement warningElement = driver.findElement(warningMessageBy);
         assertTrue(warningElement.isDisplayed());
+
         String expected = "Invalid user name or password.";
         String actual = warningElement.getText();
-        assertEquals(actual,expected);
-
+        assertEquals(actual, expected);
     }
+
     @Test(description = "Login as store manager and verify that tile is equals to Dashboard")
-    public void loginAsStoreManger(){
+    public void loginAsStoreManager(){
         driver.findElement(usernameBy).sendKeys(username);
-        driver.findElement(passwordBy).sendKeys(password,Keys.ENTER);
-        BrowserUtils.wait(4);
+        driver.findElement(passwordBy).sendKeys(password, Keys.ENTER);
+        BrowserUtils.wait(5);
 
         String expected = "Dashboard";
         String actual = driver.getTitle();
 
-        assertEquals(actual,expected,"Page title is not correct!");
-
+        assertEquals(actual, expected, "Page title is not correct!");
     }
 
 
     @BeforeMethod
-    public void setup(){
+    public void setup() {
         WebDriverManager.chromedriver().version("79").setup();
         driver = new ChromeDriver();
         driver.get(URL);
         driver.manage().window().maximize();
     }
+
     @AfterMethod
-    public void tearDown(){
+    public void teardown() {
         //if webdriver object alive
-        if (driver != null){
+        if (driver != null) {
             //close browser, close session
             driver.quit();
-            //destroy driver object for sure
-            driver=null;
+            //destroy webdriver object for sure
+            driver = null;
         }
     }
-
-
-
-
-
-
+/*
+html5 warning message attribute
+this warning message can not be catched with locator
+it some kind of attribute coming from html
+you can not catch it, there is a method getAttribute
+you can just read it
+ */
 
 }
