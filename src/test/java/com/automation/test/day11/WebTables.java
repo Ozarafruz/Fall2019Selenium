f7c52955de35bf28078ab151e0520e32015b6652
ozarafruz <ozarafruz@gmail.com>
2020-03-21 16:23:06 -0400

    Web Tables Intro

--- src/test/java/com/automation/test/day11/WebTables.java
+++ src/test/java/com/automation/test/day11/WebTables.java
@@ -2,9 +2,12 @@ package com.automation.test.day11;
 
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -20,7 +23,15 @@ public class WebTables {
 
     @BeforeMethod
     public void setup() {
-        driver = DriverFactory.createDriver("chrome");
+
+        WebDriverManager.chromedriver().version("79").setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        //headless mode makes execution twice faster
+        //it does everything except file uploading
+        //set it to tru to make it work
+        chromeOptions.setHeadless(false);//to run browser without GUI. Makes browser invisible.
+        driver = new ChromeDriver(chromeOptions);
+
         driver.get("http://practice.cybertekschool.com/tables");
         driver.manage().window().maximize();
         BrowserUtils.wait(3);
@@ -44,9 +55,26 @@ public class WebTables {
         for (WebElement columnName : columnNames) {
             System.out.println(columnName.getText());
         }
-        Assert.assertEquals(BrowserUtils.getTextFromWEbElements(columnNames),expected);
+        Assert.assertEquals(BrowserUtils.getTextFromWebElements(columnNames),expected);
     }
+    @Test
+    public void verifyRowCount() {
+        ////tbody//tr - to get all rows from table body, excluding table header
+        List<WebElement> rows = driver.findElements(By.xpath("//table[1]//tbody//tr"));
+        //if we will get a size of this collection, it automatically equals to number of elements
+        //expected - 4 rows
+        Assert.assertEquals(rows.size(), 4);
 
+    }
+    /**
+     * To get specific column, skip row index, and just provide td index
+     */
+    @Test
+    public void getSpecificColumn(){
+        //td[5] - column with links
+        List<WebElement> links = driver.findElements(By.xpath("//table[1]//tbody//tr//td[5]"));
+        System.out.println(BrowserUtils.getTextFromWebElements(links));
+    }
 
 
 
