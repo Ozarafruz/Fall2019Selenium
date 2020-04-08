ae3bb79e25721a8f9acdfe456e4df99529f5b7d5
ozarafruz <ozarafruz@gmail.com>
2020-04-07 22:38:50 -0400

    DAY 25 / Import Excel from Downloads file to IntelliJ / ExcelUtil created / Excel io / Update

--- src/test/java/com/automation/test/vytrack/AbstractTestBase.java
+++ src/test/java/com/automation/test/vytrack/AbstractTestBase.java
@@ -18,71 +18,66 @@ import java.io.IOException;
 
 public abstract class AbstractTestBase {
     //will be visible in the subclass, regardless on subclass location (same package or no)
-
     protected WebDriverWait wait;
     protected Actions actions;
 
     protected ExtentReports report;
     protected ExtentHtmlReporter htmlReporter;
-    protected ExtentTest test ;
+    protected ExtentTest test;
 
-    //@Optional - to make parameter Optional
-    //if you don't specify it, testing will require to specify this parameter for every test, in xml runner
-
+    //@Optional - to make parameter optional
+    //if you don't specify it, testng will require to specify this parameter for every test, in xml runner
     @BeforeTest
     @Parameters("reportName")
-    public void setupTest(@Optional String reportName){
-        System.out.println("Report name: "+reportName);
-        report = new ExtentReports();
-        reportName = reportName == null ? "report.html" : reportName;
+    public void setupTest(@Optional String reportName) {
+        System.out.println("Report name: " + reportName);
+        reportName = reportName == null ? "report.html" : reportName + ".html";
 
-      //  String reportPath = System.getProperty("user.dir")+"/test-output/report.html";
+        report = new ExtentReports();
+
         String reportPath = "";
         //location of report file
-        if (System.getProperty("os.name").toLowerCase().contains("win")){
-            reportPath = System.getProperty("user.dir")+"\\test-output\\"+reportName;
-        }else {
-            reportPath = System.getProperty("user.dir")+"/test-output/"+ reportName;
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            reportPath = System.getProperty("user.dir") + "\\test-output\\" + reportName;
+        } else {
+            reportPath = System.getProperty("user.dir") + "/test-output/" + reportName;
         }
-        //is a HTML itself
+        //is a HTML report itself
         htmlReporter = new ExtentHtmlReporter(reportPath);
-        //add it to reporter
+        //add it to the reporter
         report.attachReporter(htmlReporter);
-        htmlReporter.config().setReportName("VYTRACK Test Automation Result");
-    }
-@AfterTest
-    public void afterTest(){
-        report.flush(); // to release report
-
+        htmlReporter.config().setReportName("VYTRACK Test Automation Results");
     }
 
+    @AfterTest
+    public void afterTest() {
+        report.flush();//to release a report
+    }
 
     @BeforeMethod
-    public void setup(){
-        String URL = ConfigurationReader.getProperty("qa1");
+    public void setup() {
+        String URL = ConfigurationReader.getProperty("qa3");
         Driver.getDriver().get(URL);
         Driver.getDriver().manage().window().maximize();
-        wait = new WebDriverWait(Driver.getDriver(), 15);
+        wait = new WebDriverWait(Driver.getDriver(), 25);
         actions = new Actions(Driver.getDriver());
     }
+
 
     @AfterMethod
     public void teardown(ITestResult iTestResult) throws IOException {
         //ITestResult class describes the result of a test.
         //if test failed, take a screenshot
-        //no failure no screenshot
+        //no failure - no screenshot
         if (iTestResult.getStatus() == ITestResult.FAILURE) {
             //screenshot will have a name of the test
             String screenshotPath = BrowserUtils.getScreenshot(iTestResult.getName());
-            test.addScreenCaptureFromPath(screenshotPath);//attach screenshot
-            BrowserUtils.wait(5);
             test.fail(iTestResult.getName());//attach test name that failed
+            BrowserUtils.wait(2);
+            test.addScreenCaptureFromPath(screenshotPath, "Failed");//attach screenshot
             test.fail(iTestResult.getThrowable());//attach console output
         }
-
         BrowserUtils.wait(2);
         Driver.closeDriver();
     }
-
-
 }
